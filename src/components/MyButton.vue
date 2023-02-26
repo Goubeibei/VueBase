aeed54d22b4b820b2722ccd2c84ef874e0484548
Goubeibei <794043606@qq.com>
2023-02-26 10:38:32 +0800

    first

---
+++ src/components/MyButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>goubei</h2>
+    <button @click="count++">笑了{{count}}</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'',
+    data() {
+      return {
+        count:0
+      }
+    },
+  }
+</script>
+
+<style scoped>
+h2{
+  color:aqua
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// import Vue from "vue/dist/vue.esm.js";
+import App from '@/App';
+
+import Vue from 'vue'
+
+// new Vue({
+//   el:'#app',
+//   components:{
+//     App,
+//   },
+//   template:'<App/>'
+// })
+
+
+new Vue({
+  el:'#app',
+  render:h => h(App)
+})
